5c4d1c3d4bad5f3462dfdf174d5f3a434dbbe970
Alessandro Mangone BID <alessandro.mangone@bidcompany.it>
2024-07-06 15:11:02 +0200

    add mutations

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,67 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-import { typeDefs } from "./schema";
+import db from "./db.js";
+import { typeDefs } from "./schema.js";
+
+const resolvers = {
+    Query: {
+        reviews: () => db.reviews,
+        games: () => db.games,
+        authors: () => db.authors,
+        review: (_, { id }) => db.reviews.find(review => review.id === id),
+        game: (_, { id }) => db.games.find(game => game.id === id),
+        author: (_, { id }) => db.authors.find(author => author.id === id),
+    },
+    // parent is the object that contains the field being resolved
+    // in this case, parent is the game object that contains the reviews field
+    Game: {
+        reviews: (parent) => db.reviews.filter(review => review.game_id === parent.id),
+    },
+    Author: {
+        reviews: (parent) => db.reviews.filter(review => review.author_id === parent.id),
+    },
+    Review: {
+        // find because there is only one author per review
+        author: (parent) => db.authors.find(author => author.id === parent.author_id),
+        game: (parent) => db.games.find(game => game.id === parent.game_id)
+    },
+    Mutation: {
+        deleteGame: (_, { id }) => {
+            db.games = db.games.filter(game => game.id !== id);
+            return db.games;
+        },
+        addGame: (_, { game }) => {
+            const newGame = {
+                id: Math.floor(Math.random() * 1000).toString(),
+                ...game
+            }
+            db.games.push(newGame);
+            return newGame;
+        },
+        updateGame: (_, { id, edits }) => {
+            db.games = db.games.map(game => {
+                if (game.id === id) {
+                    return {
+                        ...game,
+                        ...edits
+                    }
+                }
+                return game;
+            })
+
+            return db.games.find(game => game.id === id); // because the return type is Game
+        }
+    }
+}
 
 // server setup
-
 const APOLLO_PORT = 4000
+// typeDefs --  definition of types of data
+// resolvers -- functions that return data for the schema
 const server = new ApolloServer({
-    // typeDefs --  definition of types of data
     typeDefs,
-    // resolvers -- functions that return data for the schema
+    resolvers,
 })
 
 const { url } = await startStandaloneServer(server, {
--- src/schema.ts
+++ src/schema.ts
@@ -1,29 +1,53 @@
 // int, float, string, boolean, ID data types
 
+/* esclamation mark means field is required */
+/* Query type is the entry point for all GraphQL queries */
 export const typeDefs = `#graphql
     type Game {
-        id: ID! /* esclamation mark means field is required */
+        id: ID! 
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
-        reviews: [Review!]!
+        reviews: [Review!]
     }
 
-    /* Query type is the entry point for all GraphQL queries */
+    
     type Query {
-        reviews: [Reviews]
+        reviews: [Review]
+        review(id: ID!): Review
         games: [Game]
+        game(id: ID!): Game
         authors: [Author]
+        author(id: ID!): Author
+    }
+
+    type Mutation {
+        deleteGame(id: ID!): [Game]
+        addGame(game: AddGameInput!): Game
+        updateGame(id: ID!, edits: EditGameInput!): Game
+    }
+
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
+
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 `
